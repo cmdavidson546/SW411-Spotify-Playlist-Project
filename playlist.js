f063b1a4c8c5f4a6b642630c95eb24a23acf245c
Chris Davidson <47957304+cmdavidson546@users.noreply.github.com>
2019-04-09 20:19:12 -0400

    Version 5.0

--- playlist.js
+++ playlist.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static("public"));
+app.use(express.static("public"));  // put css in /public storage
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");  
@@ -19,51 +19,78 @@ app.post("/", function(req, res) {
     var status = res.statusCode;
     console.log(status);
 
-    var user_name = req.body.name;
     var email = req.body.email;
-    var uStation = req.body.station;
+    
+    // get artist list from user
+    var artists = req.body.artist;
+    console.log(artists);
+    
+    // split list into various artists
+    artists = artists.split(',');
+    console.log(artists);
+    
+    // get size for reference
+    var size = artists.length;
+    console.log(size);
+    
+    
+    var region = req.body.region;   
+    // convert: east(=NY), west(=LA), south(=Atlanta), central(=Chicago)
+    console.log(region);
+    
+    // use for timer
     var timer2 = req.body.timer_drop;
     
     async function main() {
+        const matches = await iheart.search(artists[0]);    // search 1st selected artist from arraylist
+        var test = JSON.stringify(matches);
         
-        const matches = await iheart.search(uStation);
-        var stationData = JSON.stringify(matches);
-         
-         fs.writeFile('iheartRETURN.json', stationData, function (err) {
-            if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end("404 Not Found");
-            }
-            console.log('Saved!');
-        });
-        /*
-        // OPEN SPOTIFY LOGIN PAGE - DOES NOT WORK
-        // NEED TO CONNECT THE DESTINATION PAGE BACK TO SERVER
-        // place inside fs.writefile function
-        fs.readFile('spotifyAuth.html', function(err, data) {
+        // GET ARTIST - use first for debugging
+        
+        const artistName = matches.artists[0].artistName;
+        const artistId = matches.artists[0].artistId;
+        console.log(artistName);
+        
+        
+        // best artist match has objType: 'ARTIST'       
+        //const artistID = matches.bestMatch.id;  // no need index for artist search
+        
+        // GET ALBUMS BY ID - 
+        // want to cycle through all albums here??
+        // in iHeart API, trackbundles == albums
+        const artistAlbum = matches.trackBundles[0].title;
+        console.log(artistAlbum);
+        
+        
+        res.write("Best Match Artist Name: " + artistName);
+        res.write("\n");
+        res.write("\n");
+
+        
+        
+        // Can make multiple calls to iheart API inside this async function
+        // this call takes the genre from FAVORITE ARTIST and gets station/artist matches
+        //const matches2 = await iheart.search(bundleGenre);
+        //var test2 = JSON.stringify(matches2);
+
+        
+        res.send();
+        
+        
+        // READ OTHER PAGES TO BOUNCE OFF OF THIS SERVER
+    /*    fs.readFile('success.html', function(err, data) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);    // respond by writing html page
             res.end();
-        });
-        */
-        
-        // RESPOND BY SENDING "SUCCESS" FILE TO USER'S BROWSER as a finale
-        // read the html page, pass to <data> obj to display
-        fs.readFile('success.html', function(err, data) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);    // respond by writing html page
-            res.end();
-        });
-        
-        
-    } 
+        }); */
+    }
+ 
     main().catch(err => {
     console.error(err);
     process.exit(1); });
     
 });
 
-// CALLBACK FROM SPOTIFY LOGIN DOES NOT WORK...need to "catch" the page here or create another page to catch http://localhost:8000/callback
 
 var port = process.env.PORT;
 if (port == null || port == "") {
@@ -74,14 +101,21 @@ app.listen(port, function() {
     console.log("Server started on port 8000");
 });
 
+// psuedo code for script...
+// allow user to enter artist names
+// go to api to get artist
+// using ids we want to get albums
+// then get tracks
+// then build playlist
 
 /*
 
-// HERE ARE INITIAL SEARCH RESULTS...header of response
+// SEARCH BY 'GENRE' (user defined) RETURNS trackbundles, artists, stations and more...
+
 { errors: null,
   duration: 183,
   bestMatch:
-   { objType: 'STATION',
+   { objType: 'STATION',    <= can we search by objType?
      id: 3278,
      confidenceVal: 0.7333333333333333 },
   bestMatches: null,
@@ -95,7 +129,7 @@ app.listen(port, function() {
   totalPrnEpisodes: null,
   totalKeywords: null,
 
-// THEN WE GET TO THE PARENT AND CHILD SEARCH TERMS returned from iHeart
+
 trackBundles:
    [ { title: 'Rocke Alliansen',
        artist: 'Various Artists',
@@ -200,5 +234,82 @@ stations:
        newlogo:
         'http://i.iheart.com/v3/re/new_assets/15a39216-5608-4302-8640-7a13c46bcd40' }, ...
 
+// SEARCH BY 'ARTIST' (user defined) ONLY RETURNS TRACKBUNDLES
+
+"trackBundles":
+[
+{
+"title":"Elvis Presley",
+"artist":"ElvisPresley",
+"albumId":22586522,
+"artistId":1014,
+"copyright":null,
+"numberOfTracks":0,
+"tracks":null,
+"streamReady":false,
+"trackBundleType":null,
+"version":null,
+"releaseDate":-434764800000,
+"numberOfVolumes":0,
+"genre":"Pop",
+"genreId":null,
+"explicitLyrics":false,
+"score":180539.17,
+"rank":null,
+"searchScore":180539.17,
+"allowStreaming":false,
+"rightsAllowed":false,
+"calculatedType":null,
+"imagePath":"http://image.iheart.com/ihr-ingestion-pipeline-production-sbmg/A10301A0003017544V_20190305013532161/58152286.20126.jpg",
+
+"recordLabelRestValue" {
+    "recordProvider":null,
+    "recordLabel":null,
+    "recordLabelId":0,
+    "recordSublabel":null 
+    },
+    
+"smallAlbumCover":null,
+"mediumAlbumCover":null,
+"largeAlbumCover":null,
+"currencyCode":null,
+"fileSize":0,
+"albumRank":null,
+"albumType":null,
+"trackOnly":false 
+},
+
+{
+"title":"Elvis Presley",
+"artist":"Elvis Presley",
+"albumId":63683325,
+"artistId":1014,
+"copyright":"Makondo",
+"numberOfTracks":0,
+"tracks":null,
+"streamReady":false,
+"trackBundleType":null,
+"version":"On Stage",
+"releaseDate":1540512000000,
+"numberOfVolumes":0,
+"genre":"Rock - Rock 'n' Roll",
+"genreId":null,
+"explicitLyrics":false,
+"score":180539.17,
+"rank":null,
+"searchScore":180539.17,
+"allowStreaming":false,
+"rightsAllowed":false,
+"calculatedType":null,
+"imagePath":"http://image.iheart.com/ihr-ingestion-pipeline-production-believe/NEW_CONTENT_DROP_HERE/20181025/3615935052559/3615935052559.jpg",
+"recordLabelRestValue":
+{"recordProvider":null,
+"recordLabel":null,"recordLabelId":0,"recordSublabel":null},
+
+"smallAlbumCover":null,"mediumAlbumCover":null,"largeAlbumCover":null,"currencyCode":null,"fileSize":0,"albumRank":null,"albumType":null,"trackOnly":false},
+
+{"title":"Elvis Presley Christmas Duets","artist":"Elvis Presley","albumId":2642436,"artistId":1014,"copyright":null,"numberOfTracks":0,"tracks":null,"streamReady":false,"trackBundleType":null,"version":null,"releaseDate":1223596800000,"numberOfVolumes":0,"genre":"Holiday","genreId":null,"explicitLyrics":false,"score":154025.11,"rank":null,"searchScore":154025.11,"allowStreaming":false,"rightsAllowed":false,"calculatedType":null,"imagePath":"http://image.iheart.com/content/music/prod/SBMG7/Thumb_Content/Full_PC/SBMG/Jan15/011514/A10301A0001662949Q_20150114203232783/resources/A10301A0001662949Q_T-10202_Image.jpg","recordLabelRestValue":
+
+{"recordProvider":null,"recordLabel":null,"recordLabelId":0,"recordSublabel":null},"smallAlbumCover":null,"mediumAlbumCover":null,"largeAlbumCover":null,"currencyCode":null,"fileSize":0,"albumRank":null,"albumType":null,"trackOnly":false}
 
 */
